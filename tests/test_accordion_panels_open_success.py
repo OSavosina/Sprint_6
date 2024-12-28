efa758b6f24376b4f0dd9c2f17d9fbd173cf7b2c
OSavosina <kcuxa4@yandex.ru>
2024-12-28 16:05:06 +0300

    Update test_accordion_panels_open_success.py
    Change test_accordion_panels_open_success

--- tests/test_accordion_panels_open_success.py
+++ tests/test_accordion_panels_open_success.py
@@ -1,13 +1,17 @@
 import pytest
+from page_objects.main_page import MainPage
+from data import AccordionConstants
 from conftest import driver
-from page_objects.accordion_questions import AccordionQuestions, Constants
+import allure
 
-accordion_heading = list(range(Constants.ACCORDION_HEADING_COUNT)) # Количество панелей
-panel_texts = Constants.ACCORDION_PANELS_TEXTS # Список с текстами выпадающих панелей
 
 class TestAccordionPanelOpenSuccess:
-    @pytest.mark.parametrize('item', accordion_heading)
+    @allure.title('Открытие панелей с вопросами')
+    @pytest.mark.parametrize('item', list(range(AccordionConstants.ACCORDION_HEADING_COUNT)))
     def test_accordion_panel_open_success(self, driver, item):
-            accordion_questions = AccordionQuestions(driver)
-            accordion_questions.check_accordion_panel_is_enabled(item, panel_texts[item])
+        accordion_questions = MainPage(driver)
+        expected_value = AccordionConstants.ACCORDION_PANELS_TEXTS[item]
+        actually_value = accordion_questions.check_accordion_panel_is_enabled(item, AccordionConstants.ACCORDION_PANELS_TEXTS[item])
+        assert actually_value == expected_value, f'Ожидалось значение: "{expected_value}", получено "{actually_value}"'
+
 
