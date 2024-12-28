6adc74e8d845a7986b8dba44e236e4cc25590231
OSavosina <kcuxa4@yandex.ru>
2024-12-28 16:06:26 +0300

    Update test_make_order_by_button_success.py
    Change test_make_order_by_button_success

--- tests/test_make_order_by_button_success.py
+++ tests/test_make_order_by_button_success.py
@@ -1,20 +1,22 @@
+import allure
 import pytest
 from conftest import driver
-from page_objects.form_order import FormOrder
-from page_objects.form_order import UserInfoConstants
+from page_objects.order_page import OrderPage
+from data import UserInfoConstants
 from page_objects.main_page import MainPage
+from locators import Locators
 
 
 class TestMakeOrderByButtonSuccess:
-     button_top = MainPage.locator_top_button
-     button_bottom = MainPage.locator_bottom_button
-
-     @pytest.mark.parametrize('order_data,button', [(UserInfoConstants.data_1, button_top), (UserInfoConstants.data_2, button_bottom)])
+     @allure.title('Оформление заказа')
+     @pytest.mark.parametrize('order_data,button', [(UserInfoConstants.data_1, Locators.locator_top_button), (UserInfoConstants.data_2, Locators.locator_bottom_button)])
      def test_make_order_by_button_success(self, driver, order_data, button):
-          form_order = FormOrder(driver)
+          form_order = OrderPage(driver)
           main_page = MainPage(driver)
 
-          main_page.get_cookies()
+          main_page.accept_cookies()
           main_page.click_button_order(button)
 
-          form_order.make_order(order_data)
+          order_made_success = form_order.make_order(order_data)
+          assert order_made_success == True
+          
